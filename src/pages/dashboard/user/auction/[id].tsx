cc89dbc153509b526e25288597e09ea96a563d12
Naïm Abdelkerim <naimdevelopper@gmail.com>
2023-06-19 12:21:10 +0100

    garage view

--- src/pages/dashboard/user/auction/[id].tsx
+++ src/pages/dashboard/user/auction/[id].tsx
@@ -53,7 +53,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   if (!session) {
     return {
       redirect: {
-        destination: "/",
+        destination: "/auth/login",
         permanent: true,
       },
     };
