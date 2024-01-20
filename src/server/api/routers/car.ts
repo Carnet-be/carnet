092b53a5860c9593194e896e777d1506d29e367a
Naïm Abdelkerim <naimdevelopper@gmail.com>
2024-01-20 15:24:05 +0100

    changing cars structure

--- src/server/api/routers/car.ts
+++ src/server/api/routers/car.ts
@@ -9,189 +9,175 @@ import {
   getTableColumns,
   sql,
   type InferInsertModel,
-  Table,
+  type InferSelectModel,
 } from "drizzle-orm";
 
 import { TRPCClientError } from "@trpc/client";
 import { z } from "zod";
-import { carAssets } from "~/server/db/schema/assets";
-import auctionDetails from "~/server/db/schema/auction_details";
+import { assets } from "~/server/db/schema/assets";
 import body from "~/server/db/schema/bodies";
 import brand from "~/server/db/schema/brands";
 import carOption, { type CarOption } from "~/server/db/schema/car_options";
-import carSpecs, { type CarSpecs } from "~/server/db/schema/car_specs";
-import carSpecsRating from "~/server/db/schema/car_specs_rating";
 import carToOption from "~/server/db/schema/car_to_optons";
 import cars from "~/server/db/schema/cars";
 import colors from "~/server/db/schema/colors";
 import model from "~/server/db/schema/models";
 import { type FullCar } from "~/types";
-import { createInsertSchema } from "drizzle-zod";
 
-id: int("id").autoincrement().primaryKey(),
-belongsTo: varchar("belongsTo", { length: 255 }).notNull(),
-state: mysqlEnum("state", ["new", "used"]),
-type: mysqlEnum("type", ["auction", "direct"]).notNull(),
-name: varchar("name", { length: 255 }).notNull(),
-createdAt: timestamp("created_at").defaultNow(),
-updatedAt: timestamp("updated_at").onUpdateNow(),
-description: text("description"),
-bodyId: int("body_id"),
-brandId: int("brand_id").notNull(),
-modelId: int("model_id"),
-year: int("year"),
-colorId: int("color"),
-fuel: mysqlEnum("fuel", ["gasoline", "diesel", "electricity", "hybrid"]),
-status: mysqlEnum("status", [
-  "pending",
-  "published",
-  "paused",
-  "finished",
-  "completed",
-  "sold",
-])
-  .notNull()
-  .default("pending"),
-statusChangedAt: timestamp("status_changed_at"),
-minPrice: float("min_price"),
-maxPrice: float("max_price"),
-inRange: boolean("in_range").default(false),
-price: float("price"),
-countryId: int("country_id"),
-cityId: int("city_id"),
-address: varchar("address", { length: 255 }),
-lat: float("lat"),
-lon: float("lon"),
-zipCode: varchar("zip_code", { length: 15 }),
-startingPrice: float("starting_price").default(0.0),
-commission: float("commission").default(0.0),
-duration: mysqlEnum("duration", ["3d", "7d", "14d", "30d"]).default("3d"),
-expectedPrice: float("expected_price").default(0.0),
-startedAt: timestamp("started_at"),
-endedAt: timestamp("ended_at"),
-handling: int("handling"),
-tires: int("tires"),
-exterior: int("exterior"),
-interior: int("interior"),
-transmission: mysqlEnum("transmission", [
-  "manual",
-  "automatic",
-  "semi-automatic",
-]),
-doors: int("doors"),
-cv: float("cv"),
-cc: float("cc"),
-co2: float("co2"),
-mileage: float("kilometrage"),
-version: varchar("version", { length: 255 }),
-const addCar = protectedProcedure.input().mutation(async ({ input, ctx }) => {
-  //TODO: add state and description
-  const { db, auth } = ctx;
-  const belongsTo = (auth.orgId ?? auth.userId)!;
-  const {
-    color = null,
-    brand: bd,
-    model: ml,
-    fuel = null,
-    year = null,
-    state = null,
-  } = input.step1;
-  const { type, minPrice, maxPrice, inRange, price, startingPrice, duration } =
-    input.step6;
-
-  const {
-    city,
-    country,
-    address,
-    zipCode,
-    pos,
-    description = null,
-  } = input.step5;
-  const lat = pos?.lat ?? null;
-  const lon = pos?.lng ?? null;
-  const { handling, interior, exterior, tires } = input.step4;
-  const { options } = input.step3;
-  const {
-    cc = null,
-    cv = null,
-    co2 = null,
-    mileage = null,
-    version = null,
-    body,
-    transmission = null,
-    doors = null,
-  } = input.step2;
-
-  return db.transaction(async (trx) => {
-    const [name] = await trx
-      .select({
-        brandName: brand.name,
-        modelName: model.name,
-      })
-      .from(brand)
-      .innerJoin(model, eq(brand.id, model.brandId))
-      .where(and(eq(brand.id, bd), eq(model.id, ml)));
-
-    const auctionDetail = await trx.insert(auctionDetails).values({
-      startingPrice,
-      duration,
-    });
-    const auctionDetailsId = parseInt(auctionDetail.insertId);
-
-    const carData: InferInsertModel<typeof cars> = {
-      colorId: color,
-      brandId: bd,
-      modelId: ml,
-      fuel,
-      state,
-      year,
+export const createCarSchema = z.object({
+  step1: z.object({
+    color: z.number().nullable(),
+    brand: z.number(),
+    model: z.number(),
+    fuel: z.enum(["gasoline", "diesel", "electricity", "hybrid"]).nullable(),
+    year: z.number().nullable(),
+    state: z.enum(["new", "used"]).nullable(),
+  }),
+  step2: z.object({
+    cc: z.number().nullable(),
+    cv: z.number().nullable(),
+    co2: z.number().nullable(),
+    mileage: z.number().nullable(),
+    version: z.string().nullable(),
+    body: z.number(),
+    transmission: z.enum(["manual", "automatic", "semi-automatic"]).nullable(),
+    doors: z.number().nullable(),
+  }),
+  step3: z.object({
+    options: z.array(z.number()),
+  }),
+  step4: z.object({
+    handling: z.number(),
+    interior: z.number(),
+    exterior: z.number(),
+    tires: z.number(),
+  }),
+  step5: z.object({
+    city: z.number(),
+    country: z.number(),
+    address: z.string(),
+    zipCode: z.string(),
+    pos: z.object({
+      lat: z.number(),
+      lng: z.number(),
+    }),
+    description: z.string().nullable(),
+  }),
+  step6: z.object({
+    type: z.enum(["auction", "direct"]),
+    minPrice: z.number(),
+    maxPrice: z.number(),
+    inRange: z.boolean(),
+    price: z.number(),
+    startingPrice: z.number(),
+    duration: z.enum(["3d", "7d", "14d", "30d"]),
+  }),
+});
+const addCar = protectedProcedure
+  .input(createCarSchema)
+  .mutation(async ({ input, ctx }) => {
+    //TODO: add state and description
+    const { db, auth } = ctx;
+    const belongsTo = (auth.orgId ?? auth.userId)!;
+    const {
+      color = null,
+      brand: bd,
+      model: ml,
+      fuel = null,
+      year = null,
+      state = null,
+    } = input.step1;
+    const {
       type,
-      name: `${name?.brandName} ${name?.modelName}`,
-      belongsTo,
       minPrice,
       maxPrice,
       inRange,
       price,
-      bodyId: body,
-      cityId: city,
-      countryId: country,
+      startingPrice,
+      duration,
+    } = input.step6;
+
+    const {
+      city,
+      country,
       address,
       zipCode,
-      lat,
-      lon,
-      auctionDetailsId,
-      description,
-    };
-    const car = await trx.insert(cars).values(carData);
-    if (options.length > 0)
-      await trx.insert(carToOption).values(
-        options.map((opt) => ({
-          carId: parseInt(car.insertId),
-          optionId: opt,
-        })),
-      );
-
-    await trx.insert(carSpecs).values({
-      cc,
-      cv,
-      co2,
-      mileage,
-      carId: parseInt(car.insertId),
+      pos,
+      description = null,
+    } = input.step5;
+    const lat = pos?.lat ?? null;
+    const lon = pos?.lng ?? null;
+    const { handling, interior, exterior, tires } = input.step4;
+    const { options } = input.step3;
+    const {
+      cc = null,
+      cv = null,
+      co2 = null,
+      mileage = null,
+      version = null,
       body,
-      version,
-      transmission: transmission as any,
-      doors: doors ? parseInt(doors as string) : null,
+      transmission = null,
+      doors = null,
+    } = input.step2;
+
+    return db.transaction(async (trx) => {
+      const [name] = await trx
+        .select({
+          brandName: brand.name,
+          modelName: model.name,
+          year: model.year,
+        })
+        .from(brand)
+        .innerJoin(model, eq(brand.id, model.brandId))
+        .where(and(eq(brand.id, bd), eq(model.id, ml)));
+
+      const carData: InferInsertModel<typeof cars> = {
+        colorId: color,
+        brandId: bd,
+        modelId: ml,
+        fuel,
+        state,
+        year,
+        type,
+        name: `${name?.brandName} ${name?.modelName} ${name?.year}`,
+        belongsTo,
+        minPrice,
+        maxPrice,
+        inRange,
+        price,
+        bodyId: body,
+        cityId: city,
+        countryId: country,
+        address,
+        zipCode,
+        lat,
+        lon,
+        description,
+        startingPrice,
+        duration,
+        cc,
+        cv,
+        co2,
+        mileage,
+        version,
+        transmission,
+        doors,
+        handling,
+        interior,
+        exterior,
+        tires,
+      };
+      const car = await trx.insert(cars).values(carData);
+      if (options.length > 0)
+        await trx.insert(carToOption).values(
+          options.map((opt) => ({
+            carId: parseInt(car.insertId),
+            optionId: opt,
+          })),
+        );
+      return parseInt(car.insertId);
     });
-    await trx.insert(carSpecsRating).values({
-      handling,
-      interior,
-      exterior,
-      tires,
-      carId: parseInt(car.insertId),
-    });
-    return parseInt(car.insertId);
   });
-});
 
 const addAssets = protectedProcedure
   .input(
@@ -203,11 +189,12 @@ const addAssets = protectedProcedure
   .mutation(async ({ input, ctx }) => {
     const { db } = ctx;
     const { images, carId } = input;
-    const assets: InferInsertModel<typeof carAssets>[] = images.map((img) => ({
-      carId,
+    const result: InferInsertModel<typeof assets>[] = images.map((img) => ({
+      ref: carId,
       key: img,
+      type: "image",
     }));
-    return assets.length > 0 ? db.insert(carAssets).values(assets) : [];
+    return result.length > 0 ? db.insert(assets).values(result) : [];
   });
 
 const getCars = protectedProcedure.query(async ({ ctx }) => {
@@ -216,26 +203,20 @@ const getCars = protectedProcedure.query(async ({ ctx }) => {
     .select({
       ...getTableColumns(cars),
       images: sql<
-        CarSpecs[]
-      >`IF(COUNT(${carAssets.id}) = 0, JSON_ARRAY(), json_arrayagg(json_object('id',${carAssets.id},'key',${carAssets.key})))`,
-      auctionDetails: auctionDetails,
+        InferSelectModel<typeof assets>[]
+      >`IF(COUNT(${assets.id}) = 0, JSON_ARRAY(), json_arrayagg(json_object('id',${assets.id},'key',${assets.key})))`,
       brand: brand,
       model: model,
-      specs: carSpecs,
-      specsRating: carSpecsRating,
       body: body,
       color: colors,
     })
     .from(cars)
-    .leftJoin(carAssets, eq(cars.id, carAssets.carId))
-    .leftJoin(auctionDetails, eq(cars.auctionDetailsId, auctionDetails.id))
+    .leftJoin(assets, eq(cars.id, assets.ref))
     .leftJoin(brand, eq(cars.brandId, brand.id))
     .leftJoin(model, eq(cars.modelId, model.id))
-    .leftJoin(carSpecs, eq(cars.id, carSpecs.carId))
-    .leftJoin(carSpecsRating, eq(cars.id, carSpecsRating.carId))
     .leftJoin(colors, eq(cars.colorId, colors.id))
     .leftJoin(body, eq(cars.bodyId, body.id))
-    .groupBy(() => [cars.id, carSpecs.id, carSpecsRating.id]);
+    .groupBy(() => [cars.id]);
 
   console.log("result", result);
   return result;
@@ -250,13 +231,12 @@ const getCarById = publicProcedure
       .select({
         ...getTableColumns(cars),
         images: sql<
-          CarSpecs[]
-        >`IF(COUNT(${carAssets.id}) = 0, JSON_ARRAY(), json_arrayagg(json_object('id',${carAssets.id},'key',${carAssets.key})))`,
-        auctionDetails: auctionDetails,
+          InferSelectModel<typeof assets>[]
+        >`IF(COUNT(${assets.id}) = 0, JSON_ARRAY(), json_arrayagg(json_object('id',${assets.id},'key',${assets.key})))`,
+
         brand: brand,
         model: model,
-        specs: carSpecs,
-        specsRating: carSpecsRating,
+
         body: body,
         color: colors,
         options: sql<
@@ -264,18 +244,15 @@ const getCarById = publicProcedure
         >`IF( COUNT(${carOption.id}) = 0, JSON_ARRAY(),json_arrayagg(json_object('id',${carOption.id},'name',${carOption.name})))`,
       })
       .from(cars)
-      .leftJoin(carAssets, eq(cars.id, carAssets.carId))
-      .leftJoin(auctionDetails, eq(cars.auctionDetailsId, auctionDetails.id))
+      .leftJoin(assets, eq(cars.id, assets.ref))
       .leftJoin(brand, eq(cars.brandId, brand.id))
       .leftJoin(model, eq(cars.modelId, model.id))
-      .leftJoin(carSpecs, eq(cars.id, carSpecs.carId))
-      .leftJoin(carSpecsRating, eq(cars.id, carSpecsRating.carId))
       .leftJoin(carToOption, eq(cars.id, carToOption.carId))
       .leftJoin(carOption, eq(carToOption.optionId, carOption.id))
       .leftJoin(colors, eq(cars.colorId, colors.id))
       .leftJoin(body, eq(cars.bodyId, body.id))
       .where(eq(cars.id, input))
-      .groupBy(() => [cars.id, carSpecs.id, carSpecsRating.id]);
+      .groupBy(() => [cars.id]);
     if (!result) new TRPCClientError("Car not found");
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
 
--- src/server/db/schema/models.ts
+++ src/server/db/schema/models.ts
@@ -11,12 +11,13 @@ const models = mysqlTable(
   "models",
   {
     id: int("id").autoincrement().primaryKey(),
-    name: varchar("name", { length: 255 }).notNull().unique(),
+    name: varchar("name", { length: 255 }).notNull(),
     brandId: int("brand_id").notNull(),
     createdAt: timestamp("created_at").defaultNow(),
+    year: int("year"),
   },
   (table) => ({
-    unq: unique().on(table.name, table.brandId),
+    unq: unique().on(table.name, table.year),
   }),
 );
 export default models;
